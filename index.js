c9912ea3599063767890c7c5ce8d6b1858b5b819
chaohuichen <chaohuichen@github.com>
2020-05-08 13:37:00 -0400

    finish

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 let score = 0;
+const gameArea = document.getElementById('whack-a-mole');
+const scoreDom = document.getElementById('score');
+let holes = document.getElementsByClassName('hole');
+
+setInterval(() => {
+  const randomIdx = Math.floor(Math.random() * holes.length);
+  holes[randomIdx].classList.toggle('mole');
+}, 300);
+
+gameArea.addEventListener('click', function (clickEvent) {
+  if (clickEvent.target.matches('.mole')) {
+    clickEvent.target.classList.remove('mole');
+    score++;
+    scoreDom.innerText = score;
+  }
+});
